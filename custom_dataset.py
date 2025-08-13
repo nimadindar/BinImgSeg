7d1fe104a8e409f713d89f2430e9cb684e5d0555
nimadindar <nima.dindarsafa@gmail.com>
2025-08-13 11:37:15 +0200

    updated train dataset to add class label

--- custom_dataset.py
+++ custom_dataset.py
@@ -1,54 +1,41 @@
 import os
+import numpy as np
 from PIL import Image
 
+import torch
 from torch.utils.data import Dataset
 
 class ScribbleSegTrainDataset(Dataset):
-    def __init__(self, root_dir, transform=None, target_transform=None):
-        """
-        Args:
-            root_dir (str): Path to train dataset folder (contains 'images', 'scribbles', 'ground_truth')
-            transform: Transform for images and scribbles (same spatial transforms)
-            target_transform: Transform for ground truth masks
-        """
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
         self.images_dir = os.path.join(root_dir, "images")
         self.scribbles_dir = os.path.join(root_dir, "scribbles")
-        self.gt_dir = os.path.join(root_dir, "ground_truth")
-
-        self.image_files = sorted(os.listdir(self.images_dir))
-        self.scribble_files = sorted(os.listdir(self.scribbles_dir))
-        self.gt_files = sorted(os.listdir(self.gt_dir))
-
-        assert len(self.image_files) == len(self.scribble_files) == len(self.gt_files), \
-            "Mismatch between images, scribbles, and ground truth counts."
-
+        self.masks_dir = os.path.join(root_dir, "ground_truth")
         self.transform = transform
-        self.target_transform = target_transform
+
+        self.image_ids = [os.path.splitext(f)[0] for f in os.listdir(self.images_dir) if f.endswith(".jpg")]
 
     def __len__(self):
-        return len(self.image_files)
+        return len(self.image_ids)
 
     def __getitem__(self, idx):
-        img_name = self.image_files[idx]
+        img_id = self.image_ids[idx]
 
-        img_path = os.path.join(self.images_dir, img_name)
-        scribble_path = os.path.join(self.scribbles_dir, self.scribble_files[idx])
-        gt_path = os.path.join(self.gt_dir, self.gt_files[idx])
+        img_path = os.path.join(self.images_dir, img_id + ".jpg")
+        image = np.array(Image.open(img_path).convert("RGB"), dtype=np.uint8)
 
-        image = Image.open(img_path).convert("RGB")
-        scribble = Image.open(scribble_path).convert("L")
-        gt = Image.open(gt_path).convert("L")
+        scribble_path = os.path.join(self.scribbles_dir, img_id + ".png")
+        scribble = np.array(Image.open(scribble_path), dtype=np.int64)
+
+        mask_path = os.path.join(self.masks_dir, img_id + ".png")
+        mask = np.array(Image.open(mask_path), dtype=np.int64)
+
+        class_label = torch.tensor(int(mask.max() > 0), dtype=torch.long)
 
         if self.transform:
-            image = self.transform(image)
-            scribble = self.transform(scribble)  
+            image, scribble, mask = self.transform(image, scribble, mask)
 
-        if self.target_transform:
-            gt = self.target_transform(gt)
-        # else:
-        #     gt = transforms.ToTensor()(gt)  # default: convert to tensor in [0,1]
-
-        return image, scribble, gt
+        return image, scribble, mask, class_label
 
 
 class ScribbleSegTestDataset(Dataset):
